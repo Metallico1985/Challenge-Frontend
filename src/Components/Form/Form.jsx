39c9fee1f89d0b94507420442d4943b824f8ff57
andres cardona latorre <andrescardonalatorre@gmail.com>
2023-02-28 22:41:41 -0300

    update

--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -30,11 +30,11 @@ function Form(props) {
       <div className='formContainer'>
         <h1>Ingreso Clientes</h1>
         <form onSubmit={handleSubmit} className='formIngreso'>
-          <input value={props.infoForm ? props.infoForm.nombre : null} onChange={handleNombre} type="text" placeholder='Nombre' />
-          <input value={props.infoForm ? props.infoForm.apellido : null} onChange={handleApellido} type="text" placeholder='Apellido' />
-          <input value={props.infoForm ? props.infoForm.rut : null} onChange={handleRut} type="text" placeholder='Rut' />
-          <input value={props.infoForm ? props.infoForm.telefono : null} onChange={handleTelefono} type="text" placeholder='Telefono' />
-          <select value={props.infoForm ? props.infoForm.tipo : null} onChange={handleTipo} name='posiciones'>
+          <input DefaultValue={props.infoForm ? props.infoForm.nombre : null} onChange={handleNombre} type="text" placeholder='Nombre' />
+          <input DefaultValue={props.infoForm ? props.infoForm.apellido : null} onChange={handleApellido} type="text" placeholder='Apellido' />
+          <input DefaultValue={props.infoForm ? props.infoForm.rut : null} onChange={handleRut} type="text" placeholder='Rut' />
+          <input DefaultValue={props.infoForm ? props.infoForm.telefono : null} onChange={handleTelefono} type="text" placeholder='Telefono' />
+          <select DefaultValue={props.infoForm ? props.infoForm.tipo : null} onChange={handleTipo} name='posiciones'>
             <option disabled selected >Tipo Cliente</option>
             <option value="usuario">Usuario</option>
             <option value="empresa">Empresa</option>
